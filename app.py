9f94ea9daf9057de621b5458e8dcc15dbc702c40
chunwei-07 <loochunwei12@gmail.com>
2025-05-24 13:45:25 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def phase_1_ui():
+    st.set_page_config(page_title="PolyLingua Recommender", layout="wide")
+
+    st.title("🧠 PolyLingua Recommender: Language Learning Recommender AI")
+    st.caption("PolyLingua Recommender by Agent PolySynth & Kairo")
